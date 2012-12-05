a90e94e36e1347f2e7806dbe218bca2abc056a30
Phu Phan <Phu_Phan@playstation.sony.com>
2012-12-04 16:31:05 -0800

    Fixed some bugs added rvm rc

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-getgadgify::Application.config.session_store :cookie_store, key: '_getgadgify_session'
+Getgadgify::Application.config.session_store :cookie_store, key: '_Getgadgify_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

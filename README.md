0a0393ff6d2b8e8ef34266c5f141bb3caeb6d184
Phu Phan <Phu_Phan@playstation.sony.com>
2012-12-04 16:07:00 -0800

    Initial

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-getgadgify
+Getgadgify
 ==========
 
 Get Gadgify!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Getgadgify::Application.config.secret_token = '34de5a86ac36c41c5bdb9ddc6b86767caf34b5a8840e3fde369803cdf9efa68e4f4ea9c433df795f106c8b4209ceb65d3f5848e2a0f610db1f722ef17e8b37dd'
